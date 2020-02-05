01a74b37e45eb5224b752a7ff85301a4e9c768da
Christine Bumpous <bumpouce@gmail.com>
2020-02-05 00:37:11 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,9 @@
+def say_hello (name)
+  "Hi #{name}!"
+end
+
 def help
+
 end
 
 def play(songs)
@@ -12,5 +17,9 @@ end
 
 
 def run(songs)
+  puts "Enter your name:"
+  user = gets.strip
+  puts say_hello(user)
+  
   puts "Please enter a command:"
 end
