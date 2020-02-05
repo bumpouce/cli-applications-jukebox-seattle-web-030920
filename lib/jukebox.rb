1b64f703dd2be876b140c69e02e4dd317f0dee7c
Christine Bumpous <bumpouce@gmail.com>
2020-02-05 00:53:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,10 +11,11 @@ def play(songs)
   request_song = gets.strip
   
   songs.each_with_index do |song, index|
-    if (request_song == song) || (request_song == index)
-      return puts "Playing #{request_song}"
-    end
-    return puts "Invalid input, please try again"
+    puts "Comparing #{request_song} with #{song}, and #{index}"
+#    if (request_song == song) || (request_song == index)
+#      return puts "Playing #{request_song}"
+#    end
+#    return puts "Invalid input, please try again"
   end
 end
 
